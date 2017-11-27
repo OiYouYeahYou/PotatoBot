9a2842ec86afcffa070fc8a7361ac48d81199e4f
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2017-11-27 16:00:00 +0000

    Misc util
    Adding comments
    Renaming
    Refactioring

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,9 @@
 export function nin( key, obj ) { return !( key in obj ); }
 
+/**
+ * Safely calls a function
+ * @param cb callback
+ */
 export function callCB( cb ) {
 	if ( typeof cb === 'function' )
 		return cb;
@@ -7,6 +11,7 @@ export function callCB( cb ) {
 		return noop;
 }
 
+/** Non operation */
 // tslint:disable-next-line:no-empty
 export function noop() {}
 
@@ -14,7 +19,7 @@ export function NOGO() {
 	var that = this;
 
 	that.values = undefined;
-	that.tester = function tester( condition, msg ) {
+	that.tester = ( condition, msg ) => {
 		if ( condition ) return true;
 
 		if ( !that.values )
@@ -26,20 +31,30 @@ export function NOGO() {
 	};
 }
 
-export function validatePrefix( pfx, str ) {
+/**
+ * Tests if string is prefixed
+ * @param pfx prefix
+ * @param str text
+ */
+export function isPrefixed( pfx, str ) {
 	return (
 		typeof pfx === 'string' && pfx.length > 0 &&
 		typeof str === 'string' && str.startsWith( pfx )
 	);
 }
 
+/**
+ *
+ * @param trie
+ * @param fin
+ */
 export function quatch( trie: () => any, fin?: () => void ) {
 	try { trie(); }
 	catch ( e ) { console.log( e ); }
 	finally { callCB( fin )(); }
 }
 
-// String indexOf that returns undefined instead of -1
+/** String indexOf that returns undefined instead of -1 */
 export function indexOf(
 	str: string, search: string, position?: number
 ): number | undefined {
@@ -47,21 +62,45 @@ export function indexOf(
 	return index > 0 ? index : undefined;
 }
 
+/** Checks if env exists, and sets if they don't */
 export function setEnv() {
-	var env = process.env;
-	var keys = [
-		'discord',
-		'ritoplz',
-		'mlab',
-	];
+	var lookForKeys = () => ![ 'discord', 'ritoplz', 'mlab', ].every(
+		key => key in process.env
+	);
 
-	var test = () => !keys.every( key => key in env );
+	if ( lookForKeys() )
+		require( 'dotenv' ).config();
 
-	if ( test() ) {
-		var dotenv = require( 'dotenv' );
-			dotenv.config();
-	}
-
-	if ( test() )
+	if ( lookForKeys() )
 		throw new Error( 'env\'s seems not to be set' );
+}
+
+/**
+ * Splits string into
+ * @param text
+ */
+export function splitByFirstSpace(
+	text: string
+): [ string, string | undefined ] {
+	text = text.trim();
+
+	var indexOfFirstSpace = indexOf( text, ' ' );
+
+	var a = text.slice( 0, indexOfFirstSpace ).trim();
+	var b = indexOfFirstSpace
+		? text.slice( indexOfFirstSpace ).trim()
+		: undefined;
+
+	return [ a, b ];
+}
+
+/**
+ * Removes prefix and splits command from args
+ * @param pfx
+ * @param text
+ */
+export function splitCommandString( pfx: string, text: string) {
+	text = text.slice( pfx.length ).trim();
+
+	return splitByFirstSpace( text );
 }
