c57a5a10ab827acf807d71f9725f189db54c05b0
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-01-20 21:51:32 +0000

    Removing bind command and kindred-api package

--- src/test.ts
+++ src/test.ts
@@ -1,27 +1,8 @@
 console.log( 'This is a test; Do not adjust your set.\n\n' );
 
 import { client } from './client/client';
-import { andSummonerLeague } from './kindred';
 import { setEnv } from './util';
 
 setEnv();
 
 setTimeout( () => { client.destroy(); }, 10000 );
-andSummonerLeague(
-	'nallaj',
-	'euw',
-	err => {
-		if ( err ) {
-			console.error(
-				'Kindred encountered an error,\n'
-				+ 'It might be because Rito has expired the key, again!'
-			);
-
-			throw new Error( err );
-		}
-		else {
-			console.log( 'Kindred, ok' );
-
-		}
-	}
-);
