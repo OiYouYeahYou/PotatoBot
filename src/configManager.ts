fdbfb0c7b226867525327d88a4d6d1af4394b08a
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-01-28 23:35:48 +0000

    Adding Config Manager module

---
+++ src/configManager.ts
@@ -0,0 +1,33 @@
+import { Guild, Snowflake } from "discord.js";
+import { findGuildConfig, configLists } from "./mongoose/guild";
+import { Document } from "mongoose";
+
+export async function isFeatureEnabled(
+	guild: Guild | Snowflake, feature: string
+) {
+	return await dbChcker( guild, feature, 'features' );
+}
+
+export async function isCommandEnabled(
+	guild: Guild | Snowflake, command: string
+) {
+	return await dbChcker( guild, command, 'commands' );
+}
+
+async function dbChcker(
+	guild: Guild | Snowflake,
+	item: string,
+	thing: configLists
+): Promise<boolean> {
+	const config = await findGuildConfig( guild );
+
+	if ( !config )
+		return Promise.resolve( false )
+
+	const list: string[] = config[ thing ];
+	const containsItem = list.includes( 'all' ) || list.includes( item );
+
+	return Promise.resolve( containsItem );
+}
+
+
