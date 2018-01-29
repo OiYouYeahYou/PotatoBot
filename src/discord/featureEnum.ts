9e20d0f31b22c38f48469e261dc7f10d38b2eefe
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-01-29 20:10:13 +0000

    Adding config checking to Everyone Handling feature

---
+++ src/discord/featureEnum.ts
@@ -0,0 +1,3 @@
+export const deleteEM = 'delete_everyone_mentions';
+export const scoldEM = 'scold_everyone_mentions';
+export const dareEM = 'scold_everyone_mentions';
--- src/discord/features.ts
+++ src/discord/features.ts
@@ -1,10 +1,20 @@
 import { Message } from "discord.js";
+import { isFeatureEnabled } from "../configManager";
+import { deleteEM, scoldEM, dareEM } from "./featureEnum";
 
-export function everyoneResponse( message: Message ) {
-	if ( message.deletable )
-		message.delete();
+export async function everyoneResponse( message: Message ) {
+	const { guild, deletable } = message;
+	const isDeleting = await isFeatureEnabled( guild, deleteEM );
+	const isScolding = await isFeatureEnabled( guild, scoldEM );
 
-	message.reply( "Don't mention everyone!!", {
-		// file: "https://cdn.weeb.sh/images/ryKLMPEj-.png"
-	} );
+	if ( deletable && isDeleting )
+		await message.delete();
+
+	if ( isScolding ) {
+		const file = await isFeatureEnabled( guild, dareEM )
+			? { file: "https://cdn.weeb.sh/images/ryKLMPEj-.png", }
+			: undefined;
+
+		await message.reply( "Don't mention everyone!!", file );
+	}
 }
