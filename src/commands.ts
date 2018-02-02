fee2a412e7dbad605d8a833f8e2a8049533cd1a3
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-02-02 16:07:39 +0000

    Removing Dead code

--- src/commands.ts
+++ src/commands.ts
@@ -4,37 +4,3 @@ import { List } from './commandList';
 
 export const list = new List
 requireInFile( 'commands' );
-
-export async function subCommandHandler(
-	message: Message,
-	subModules: ISubCommands,
-	args: string,
-	noMain: boolean = true
-) {
-	const [ subCommand, subArguments ] = splitByFirstSpace( args );
-	let error, result;
-	const subCommandFunction =
-		subCommand && subModules[ subCommand.toLowerCase() ];
-
-	if ( subCommandFunction )
-		try {
-			result = await subCommandFunction( message, subArguments );
-		} catch ( err ) {
-			error = err;
-		}
-	else if ( noMain )
-		await message.reply( `Not a recocnised subcommand: ${ subCommand }` );
-
-	return [
-		subCommand,
-		subArguments,
-		subCommandFunction,
-		result,
-		error
-	];
-}
-
-type TCommandFunction = ( message: Message, args: string ) => Promise<any>;
-export interface ISubCommands {
-	[ keys: string ]: TCommandFunction;
-}
