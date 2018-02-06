89c04ae6101845535d7666ea3dc2630eee32a45d
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-02-06 16:05:39 +0000

    [Commnd] Adding update command

--- src/commands/botAdministration.ts
+++ src/commands/botAdministration.ts
@@ -1,9 +1,17 @@
 import { disconnect } from '../index'
-import { destructingReply } from '../util'
+import { destructingReply, codeWrap } from '../util'
 import { exec } from 'child_process'
 import { Message } from 'discord.js'
 import List from '../classes/List'
 import { list } from '../commands'
+import { promisify } from 'util';
+
+const promisedExec = promisify( exec )
+interface megh
+{
+	stdout: string
+	stderr: string
+}
 
 let raceLock = false
 
@@ -60,6 +68,66 @@ sub.addCommand( 'restart', {
 	help: 'Destroys the message.',
 	permission: 'master',
 } )
+
+sub.addCommand( 'update', {
+	func: async ( message: Message ) =>
+	{
+		if ( raceLock )
+			return message.reply( 'Cannot do that at his time' )
+
+		raceLock = true
+
+		console.log( 'Updating by Discord command' )
+
+		const { env: { restartCommand } } = process
+		const restartable = !!restartCommand
+
+		const msg = await message.channel.send( rsMsg( restartable ) )
+		const notification = Array.isArray( msg ) ? msg[ 0 ] : msg
+
+		const pull = await promisedExec( 'git pull' )
+		await notification.edit( rsMsg( restartable, pull ) )
+
+		const gulp = await promisedExec( 'gulp' )
+		await notification.edit( rsMsg( restartable, pull, gulp ) )
+
+		if ( restartable )
+		{
+			await notification.edit( rsMsg( restartable, pull, gulp, true ) )
+			await disconnect()
+			exec( restartCommand )
+		}
+		else
+			raceLock = false
+	},
+	help: 'Destroys the message.',
+	permission: 'master',
+} )
+
+function rsMsg(
+	restartable: boolean, git?: megh, gulp?: megh, restarting = false
+)
+{
+	const gitSanitised = git
+		? `=== out ===\n${ git.stdout }\n=== err ==\n${ git.stderr }`
+		: ''
+	const gulpSanitised = gulp
+		? `=== out ===\n${ gulp.stdout }\n=== err ==\n${ gulp.stderr }`
+		: ''
+
+	const gitMessage = `Gulp output:\n${ codeWrap( gitSanitised ) }`
+	const gulpMessage = `Gulp output:\n${ codeWrap( gulpSanitised ) }`
+
+	let restartMessage
+	if ( !restartable )
+		restartMessage = 'Restarting: `You will need to restart manually`'
+	else if ( restarting )
+		restartMessage = 'Restarting: `Restarting now`'
+	else
+		restartMessage = 'Restarting: `pending`'
+
+	return [ gitMessage, gulpMessage, restartMessage ].join( '\n\n' )
+}
 
 sub.addCommand( 'invite', {
 	func: async ( message ) =>
