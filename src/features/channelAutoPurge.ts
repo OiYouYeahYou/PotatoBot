83ac1b137e3d348a926d6f20f5e7121e62e1045c
OiYouYeahYou <OiYouYeahYou@users.noreply.github.com>
2018-02-12 21:46:30 +0000

    Allowing autoPurg() to be called for individual channels

--- src/features/channelAutoPurge.ts
+++ src/features/channelAutoPurge.ts
@@ -1,7 +1,7 @@
 import { TextChannel, GuildChannel, Message } from "discord.js";
 import { client } from "../discord/client";
 import { richEmbed } from "../discord/embed";
-import { getPurgeConfigs, IPurgeConfig, savePurgeReport } from "../mongoose/autoPurgeConfig";
+import { getPurgeConfigs, IPurgeConfig, savePurgeReport, getPurgeConfig } from "../mongoose/autoPurgeConfig";
 import { HOURS, DAYS, SECONDS, AutoPurgeInterval } from "../constants";
 import { noop, safeCallAsync } from "../util";
 
@@ -24,11 +24,13 @@ export async function initAutoPurge()
 	client.setInterval( () => safeCallAsync( autoPurge ), interval )
 }
 
-export async function autoPurge()
+export async function autoPurge( channelIDOverride?: string )
 {
 	const now = Date.now()
 	const reports: IPurgeReport[] = []
-	const configs = await getPurgeConfigs()
+	const configs = channelIDOverride
+		? await getPurgeConfig( channelIDOverride )
+		: await getPurgeConfigs()
 
 	for ( const config of configs )
 	{
